e8eccba957b6da57ba59dc1b6668e2cc1824427d
Jordan <jordynwinnie@gmail.com>
2023-08-10 23:37:37 +0800

    Cleaned and Commented code

--- main.cpp
+++ main.cpp
@@ -1,39 +1,52 @@
 ﻿#include "main.h"
 
+#define NO_SONG_PLAYING nullptr
 int main() {
+	// Added for Support for Unicode Characters
 	system("chcp 65001");
 	winrt::Windows::Media::Control::GlobalSystemMediaTransportControlsSessionManager SessionMgr(NULL);
-	if (SessionMgr == NULL) {
-		// gets SessionManager instance
-		auto session_async = SessionMgr.RequestAsync();
-		// waits 5 seconds before failure
-		if (session_async.wait_for(std::chrono::seconds{ 5 }) == winrt::Windows::Foundation::AsyncStatus::Completed) {
-			SessionMgr = session_async.GetResults();
-		}
-		else {
-			std::cout << "Couldnt request instance of Session Manager" << std::endl;
-		}
+
+	// Request for Global Session
+	auto session_async = SessionMgr.RequestAsync();
+
+	// Attempt await for 5 seconds:
+	if (session_async.wait_for(std::chrono::seconds{ 5 }) != winrt::Windows::Foundation::AsyncStatus::Completed)
+	{
+		// Exit Application if cannot find a Session:
+		std::cout << "Couldnt request instance of Session Manager" << std::endl;
+		return 0;
 	}
+
+	// Get Result of Session:
+	SessionMgr = session_async.GetResults();
+
+	// Keep track of current song
 	std::string current_song;
+
 	while (true)
 	{
-		auto sessions = SessionMgr.GetCurrentSession();
-		if (sessions == nullptr) continue;
+		winrt::Windows::Media::Control::GlobalSystemMediaTransportControlsSession sessions
+			= SessionMgr.GetCurrentSession();
 
+		if (sessions == NO_SONG_PLAYING) continue;
+
+		// Get Attempt to get Media Metadata
 		auto playbackinfo = sessions.TryGetMediaPropertiesAsync();
 
-		if (playbackinfo.wait_for(std::chrono::seconds{ 5 }) == winrt::Windows::Foundation::AsyncStatus::Completed) {
-			auto info = playbackinfo.GetResults();
-			std::string title = winrt::to_string(info.Title());
-			std::string artist = winrt::to_string(info.Artist());
+		if (playbackinfo.wait_for(std::chrono::seconds{ 5 }) != winrt::Windows::Foundation::AsyncStatus::Completed) continue;
 
-			if (current_song != title)
-			{
-				current_song.clear();
-				current_song = title;
+		auto info = playbackinfo.GetResults();
 
-				std::cout << current_song << " by " << artist << std::endl;
-			};
-		}
+		std::string title = winrt::to_string(info.Title());
+		std::string artist = winrt::to_string(info.Artist());
+
+		if (current_song != title)
+		{
+			current_song.clear();
+			current_song = title;
+
+			//Output song if song changed:
+			std::cout << current_song << " by " << artist << std::endl;
+		};
 	}
 }
