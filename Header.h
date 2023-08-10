d5ddf8aedb394a51d645f353b85ea479279a8a94
Jordan <jordynwinnie@gmail.com>
2023-08-10 23:24:59 +0800

    Modified the file structure

--- Header.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <iostream>
-#include <winrt/windows.media.h>
-#include <windows.h>
---
+++ main.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <winrt/windows.foundation.h>
+#include <winrt/windows.foundation.collections.h>
+#include <winrt/windows.media.control.h>
+#include <iostream>
